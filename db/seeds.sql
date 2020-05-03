537d7747da87de1fb9317baed7a073cfccdca786
David Ghazi <davidaghazi@gmail.com>
2020-05-02 23:47:08 -0400

    server.js, connection.js, db folders completed

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Chicken Burger");
+INSERT INTO burgers (burger_name) VALUES ("Quarter Pounders");
+INSERT INTO burgers (burger_name) VALUES ("Veggie Burger");
